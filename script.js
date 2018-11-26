cf24731087a7cb6e042e516133fcd30fbd8dd781
84003 <84003@tuta.io>
2018-11-26 11:51:06 +0100

    WEBSITE VERSION 2

--- script.js
+++ script.js
@@ -1,103 +1,44 @@
-$(document).ready(function(){
-// div 1
-	$('#ref-text-1').click(function(){
-      $('#ref-div-1').removeClass('small-solaris').addClass('big-solaris');
-	});
+$(document).ready(function () {
+		var allPs = $(this).find('#main-text p, #main-text img, #main-text figure, #main-text blockquote');
+		var allColapsed = false;
 
-	$('#ref-div-1').click(function(){
-		$('#ref-div-1').removeClass('big-solaris').addClass('small-solaris');
-	});
+		$(allPs).hide();
+		$('.footnotes').hide();
 
-	$('#ref-text-1').hover(function(){
-		$('#ref-text-1').css({'color': 'blue'});
-	});
-	$('#ref-text-1').mouseleave(function(){
-		$('#ref-text-1').css({'color': '#9ec47b'});
-	});
-// div 2
-	$('#ref-text-2').hover(function(){
-		$('#ref-item-2').css({'color': 'blue'});
-		$('#ref-text-2').css({'color': 'blue'});
-	});
-	$('#ref-item-2').hover(function(){
-		$('#ref-text-2').css({'color': 'blue'});
-		$('#ref-item-2').css({'color': 'blue'});
-	});
+//click headers show indivudal paragraphs
+    $('.headers').click(function () {
+        $(this).parent().nextAll().slideToggle(200);
+				//footnotes
+				if (allColapsed === false) {
+							allColapsed = true;
+							$('.footnotes').hide();
+						} else {
+							allColapsed = false;
+							$('.footnotes').show();
+							}
+    });
+//click #title show all pargraphs and footnotes
+		$("#title").click(function(){
+				if (allColapsed === false) {
+							allColapsed = true;
+							allPs.hide();
+							$('.footnotes').hide();
+						} else {
+							allColapsed = false;
+							allPs.show();
+							$('.footnotes').show();
+							}
 
-	$('#ref-text-2').mouseleave(function(){
-		$('#ref-item-2').css({'color': '#9ec47b'});
-		$('#ref-text-2').css({'color': '#9ec47b'});
-	});
-	$('#ref-item-2').mouseleave(function(){
-		$('#ref-text-2').css({'color': '#9ec47b'});
-		$('#ref-item-2').css({'color': '#9ec47b'});
-	});
-	$('#ref-text-3').hover(function(){
-		$('#ref-item-3').css({'color': 'blue'});
-		$('#ref-text-3').css({'color': 'blue'});
-	});
-	$('#ref-text-3').mouseleave(function(){
-		$('#ref-item-3').css({'color': '#9ec47b'});
-		$('#ref-text-3').css({'color': '#9ec47b'});
-	});
-	$('#ref-item-3').hover(function(){
-		$('#ref-text-3').css({'color': 'blue'});
-		$('#ref-item-3').css({'color': 'blue'});
-	});
-	$('#ref-item-3').mouseleave(function(){
-		$('#ref-text-3').css({'color': '#9ec47b'});
-		$('#ref-item-3').css({'color': '#9ec47b'});
-	});
-// div 3
-	$('#ref-text-4').hover(function(){
-		$('#ref-item-4').css({'color': 'blue'});
-		$('#ref-text-4').css({'color': 'blue'});
-	});
-	$('#ref-text-4').mouseleave(function(){
-		$('#ref-item-4').css({'color': '#9ec47b'});
-		$('#ref-text-4').css({'color': '#9ec47b'});
-	});
-	$('#ref-item-4').hover(function(){
-		$('#ref-text-4').css({'color': 'blue'});
-		$('#ref-item-4').css({'color': 'blue'});
-	});
-	$('#ref-item-4').mouseleave(function(){
-		$('#ref-text-4').css({'color': '#9ec47b'});
-		$('#ref-item-4').css({'color': '#9ec47b'});
-	});
-// div 4
-	$('#ref-text-5').hover(function(){
-		$('#ref-item-5').css({'color': 'blue'});
-		$('#ref-text-5').css({'color': 'blue'});
-	});
-	$('#ref-text-5').mouseleave(function(){
-		$('#ref-item-5').css({'color': '#9ec47b'});
-		$('#ref-text-5').css({'color': '#9ec47b'});
-	});
-	$('#ref-item-5').hover(function(){
-		$('#ref-text-5').css({'color': 'blue'});
-		$('#ref-item-5').css({'color': 'blue'});
-	});
-	$('#ref-item-5').mouseleave(function(){
-		$('#ref-text-5').css({'color': '#9ec47b'});
-		$('#ref-item-5').css({'color': '#9ec47b'});
-	});
-// div 5
-	$('#ref-text-6').hover(function(){
-		$('#ref-item-6').css({'color': 'blue'});
-		$('#ref-text-6').css({'color': 'blue'});
-	});
-	$('#ref-text-6').mouseleave(function(){
-		$('#ref-item-6').css({'color': '#9ec47b'});
-		$('#ref-text-6').css({'color': '#9ec47b'});
-	});
-	$('#ref-item-6').hover(function(){
-		$('#ref-text-6').css({'color': 'blue'});
-		$('#ref-item-6').css({'color': 'blue'});
-	});
-	$('#ref-item-6').mouseleave(function(){
-		$('#ref-text-6').css({'color': '#9ec47b'});
-		$('#ref-item-6').css({'color': '#9ec47b'});
-	});
+    });
+
+
+  	//change color of all headers on #title hover
+  	$(this).find('#title').hover(function(){
+  		$('#title, .headers').addClass("title-on-hover");
+  	});
+  	$(this).find('#title').mouseleave(function(){
+  		$('#title, .headers').removeClass("title-on-hover");
+  	});
+
 
 });
