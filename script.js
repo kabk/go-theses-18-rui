d2bf15aa6428426149cbe0353bd202b21f385fce
84003 <84003@tuta.io>
2018-12-10 12:35:54 +0100

    new, A4 generated print version

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
 $(document).ready(function () {
-		var allPs = $(this).find('#main-text p, #main-text img, #main-text figure, #main-text blockquote');
-		var allColapsed = false;
+	var allPs = $(this).find('#main-text p, #main-text img, #main-text figure, #main-text blockquote');
+	var allColapsed = false;
 
-		$(allPs).hide();
-		$('.footnotes').hide();
+	$(allPs).hide();
+	$('.footnotes').hide();
+	$('#info-web').hide();
+	$('#info').hide();
 
 //click headers show indivudal paragraphs
-    $('.headers').click(function () {
-        $(this).parent().nextAll().slideToggle(500);
-				//footnotes
-				if (allColapsed === false) {
-							allColapsed = true;
-							$('.footnotes').show();
-						} else {
-							allColapsed = false;
-							}
-    });
+  $('.headers').click(function () {
+    $(this).parent().nextAll().slideToggle(500);
+		if (allColapsed === false) {
+			allColapsed = true;
+			$('.footnotes').show();
+		} else {
+			allColapsed = false;
+			}
+  });
+
 //click #title show all pargraphs and footnotes
-		$("#title").click(function(){
-				if (allColapsed === false) {
-							allColapsed = true;
-							allPs.slideDown(500);
-							$('.footnotes').show();
-						} else {
-							allColapsed = false;
-							allPs.slideUp(500);
-							$('.footnotes').hide();
-							}
+	$("#title").click(function(){
+		if (allColapsed === false) {
+			allColapsed = true;
+			allPs.slideDown(500);
+			$('.footnotes').show();
+		} else {
+			allColapsed = false;
+			allPs.slideUp(500);
+			$('.footnotes').hide();
+			}
+  });
 
-    });
+	//click footnote in text show footnotes
+	$(this).find('.footnote').click(function(){
+		$('#footnotes').show();
+	});
 
-  	//change color of all headers on #title hover
-  	$(this).find('#title').hover(function(){
-  		$('#title, .headers').addClass("title-on-hover");
-  	});
-  	$(this).find('#title').mouseleave(function(){
-  		$('#title, .headers').removeClass("title-on-hover");
-  	});
+	//change color of all headers on #title hover
+	$(this).find('#title').hover(function(){
+		$('#title, .headers').addClass("title-on-hover");
+	});
+	$(this).find('#title').mouseleave(function(){
+		$('#title, .headers').removeClass("title-on-hover");
+	});
+	//click info button show info
+	$(this).find('#info-button').click(function(){
+		$('#info-web').toggle();
+		$('#footnotes').hide();
+	});
+	//click main-text hide info
+	$("#main-text").click(function(){
+		$('#info-web').hide();
+});
 
-
+$('#print-button').on('click', function() {
+  window.print();
+  return false; // why false?
+});
 
 });
